37e7a02ff4fa40e2634a0c7365ca83d6105774ff
Nikki Burnwal <nikkibarnwal44@gmail.com>
2025-02-16 16:03:19 +0530

    Mongo db connection added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import "./env.js";
+import connectDB from "./src/config/mongodb.js";
 
 const app = express();
 const PORT = process.env.PORT || 3100;
 
 app.listen(PORT, () => {
   console.log(`Server is listoning on ${PORT}`);
+  connectDB();
 });
